33f066dc61e7cf9c208fd84c1ee50a08744ca2c4
shree-77 <shreedhoni7@gmail.com>
2023-09-06 16:35:58 +0530

    app.js created and running properly

--- app.js
+++ app.js
@@ -14,3 +14,22 @@ const PORT =  process.env.PORT || 8080;
 app.listen(PORT,()=>{
     console.log(`Server is running on ${PORT} port`);
 })
+
+//middlewares
+
+app.use(express.static('public'));
+
+app.use(morgan('dev'));
+
+app.get('/',(req, res)=>{
+    
+    res.render('index',{title:'Home'});
+});
+
+app.get('/about',(req, res)=>{
+  res.render('about',{title:'About'});
+});
+
+app.get('/create',(req,res)=>{
+    res.render('create',{title:'Create'});
+})

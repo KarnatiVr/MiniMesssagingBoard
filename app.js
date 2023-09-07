eaa0e71fd1eb4badd0569c5fc6af97d823eb0401
shree-77 <shreedhoni7@gmail.com>
2023-09-07 11:18:23 +0530

    Database connected

--- app.js
+++ app.js
@@ -1,35 +1,53 @@
 const express = require('express');
 const morgan = require('morgan');
+const mongoose = require('mongoose');
 
 //Express app
 
 const app = express();
 
+//Database
+
+const dbURI = 'mongodb+srv://Shree:32Vexd9XegwTA63K@cluster0.xlxclpj.mongodb.net/MiniMessageBoard?retryWrites=true&w=majority'
+mongoose.connect(dbURI,{ useNewUrlParser: true, useUnifiedTopology: true }).then((result)=>{console.log('Connected!')})
+.catch(err=>{console.log(err)});
+
+
+//view engine
 app.set('view engine','ejs');
 
 //listen for req 
-
 const PORT =  process.env.PORT || 8080;
-
 app.listen(PORT,()=>{
     console.log(`Server is running on ${PORT} port`);
 })
 
+//register view engines
+app.set('view engine' , 'ejs');
+
 //middlewares
-
 app.use(express.static('public'));
-
 app.use(morgan('dev'));
 
-app.get('/',(req, res)=>{
-    
-    res.render('index',{title:'Home'});
-});
+//Controllers
+
+app.get('/',(req,res)=>{
+    const messages=[
+        {title: 'Batman',body :'I am Batman' },
+        {title : 'One Piece', body : 'Kaizoku-o ni orewa naru!'},
+    ];
+    res.render('pages/index',{title:'Messages',messages});
+})
+
 
 app.get('/about',(req, res)=>{
-  res.render('about',{title:'About'});
+  res.render('pages/about',{title:'About'});
 });
 
 app.get('/create',(req,res)=>{
-    res.render('create',{title:'Create'});
+    res.render('pages/create',{title:'Create'});
+})
+
+app.use((req,res)=>{
+    res.render('404',{title:'Not Found!'});
 })
